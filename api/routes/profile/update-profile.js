2348de8110411fec3e4f8110bfe51af33acf0803
Andrés Gutiérrez <andres_mau.gutierrez@uao.edu.co>
2023-01-14 17:16:18 +0000

    Arreglando el endpoint de actualización de información del usuario

--- api/routes/profile/update-profile.js
+++ api/routes/profile/update-profile.js
@@ -1,17 +1,32 @@
 import { Router } from 'express';
 import { body, check, validationResult } from 'express-validator';
 import { UserModel } from '../../models/User.js';
+import passport from 'passport';
 
 export const updateUser = Router();
 
 updateUser.put(
   '/',
   // @todo: Validación y sanitización de los datos de entrada
+  passport.authenticate('jwt', {session: false}),
 
   // @todo: Actualizar información usuario según la sesión del token JWT
   async (request, response) => {
-    return response.status(200).json({
-      //
-    });
+    try {
+      const userId = request.user.sub;
+
+      const data = await UserModel.findByIdAndUpdate({ _id: userId }, request.body);
+
+      return response.status(200).json({
+        data
+      });
+
+    } catch (error) {
+      console.error(`[Update-profile]: ${error}`);
+
+      return response.status(500).json({
+        error: 'An unexpected error happened. Please try again later',
+      });
+    }
   }
 );
